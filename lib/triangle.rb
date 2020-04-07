213a28546b710bbf948851ee34ec83b6925754ec
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-07 18:15:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,4 @@
 class Triangle
 
-  class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
-  end
   
 end
